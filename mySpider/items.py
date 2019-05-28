cf6bddb9296aab5d637370754cc5ad19c080aa0e
sunwenhao <sunwenhao@thunisoft.com>
2019-05-28 17:57:40 +0800

    SpiderDay1

---
+++ mySpider/items.py
@@ -0,0 +1,7 @@
+import scrapy
+
+
+class ItcastItem(scrapy.Item):
+    name = scrapy.Field()
+    title = scrapy.Field()
+    info = scrapy.Field()
---
+++ mySpider/spiders/itcast.py
@@ -0,0 +1,12 @@
+import scrapy
+
+
+class ItcastSpider(scrapy.Spider):
+    name = "itcast"
+    allowed_domains = ["itcast.cn"]
+    start_urls = (
+        'http://www.itcast.cn/',
+    )
+
+    def parse(self, response):
+        pass
